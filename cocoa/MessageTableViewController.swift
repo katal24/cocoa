2a1a07db7f07b44f6af00e839190ad198fc1296d
Student <student@imac-07.local>
2018-01-19 20:29:23 +0100

    almost done

--- cocoa/MessageTableViewController.swift
+++ cocoa/MessageTableViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 import Alamofire
+import DGElasticPullToRefresh
 
 class MessageTableViewController: UITableViewController {
 
     var msgs = [Message]()
+    let dateFormate = DateFormatter()
+    let urlString = "https://home.agh.edu.pl/~ernst/shoutbox.php?secret=ams2017"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let loadingView = DGElasticPullToRefreshLoadingViewCircle()
+        loadingView.tintColor = UIColor(red: 78/255.0, green: 221/255.0, blue: 200/255.0, alpha: 1.0)
+        tableView.dg_addPullToRefreshWithActionHandler({ [weak self] () -> Void in
+            
+            self?.getMessages()
+            
+            self?.tableView.dg_stopLoading()
+            }, loadingView: loadingView)
+        tableView.dg_setPullToRefreshFillColor(UIColor(red: 57/255.0, green: 67/255.0, blue: 89/255.0, alpha: 1.0))
+        tableView.dg_setPullToRefreshBackgroundColor(tableView.backgroundColor!)
         
         initMessages();
         getMessages();
@@ -29,16 +43,21 @@ class MessageTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    deinit {
+        tableView.dg_removePullToRefresh()
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+
+        
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return msgs.count
     }
     
@@ -47,17 +66,50 @@ class MessageTableViewController: UITableViewController {
     }
 
     @IBAction func addMessage(_ sender: Any) {
-        
+        let alertController = UIAlertController(title: "New message", message: "Please state your name and message", preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Your name"
+        } )
+        alertController.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Your message"
+        } )
+        let sendAction = UIAlertAction(title: "Send", style: .default, handler: { action in
+            let name = alertController.textFields?[0].text
+            let message = alertController.textFields?[1].text
+            
+            self.postMessage(name: name!, message: message!)
+        })
+        alertController.addAction(sendAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in })
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: { _ in })
+    }
+    
+    func postMessage(name: String, message: String) {
+        Alamofire.request(urlString, method: .post, parameters: ["name": name, "message": message]).responseJSON {
+            response in
+            switch response.result {
+            case .success:
+                print(response)
+                self.getMessages()
+                break
+            case .failure(let error):
+                
+                print(error)
+            }
+        }
     }
     
     
     func getMessages(){
-        Alamofire.request("https://home.agh.edu.pl/~ernst/shoutbox.php?secret=ams2017")
+      //  self.msgs = []
+        Alamofire.request(urlString)
             .responseJSON { response in
-                
+                self.msgs = []
                 if let object = response.result.value as? [String:[Any]], let messages = object["entries"] as? [[String: Any]]{
                     
                     for msg in messages {
+                        
                         self.msgs.append(Message(time: msg["timestamp"] as! String, nam: msg["name"] as! String, msg: msg["message"] as! String ))
                     }
                     
@@ -70,9 +122,18 @@ class MessageTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "messageTableViewCell", for: indexPath) as? MessageTableViewCell
         
-        let timeString = String(describing: msgs[indexPath.row].timestamp);
-        cell?.msgTime.text = timeString.substring(to: timeString.index(timeString.startIndex, offsetBy: 19))
-        cell?.msgText.text = msgs[indexPath.row].message
+       
+        
+        let diffTime = NSDate().timeIntervalSince(msgs[indexPath.row].timestamp)/60
+        
+        if(diffTime <= 200) {
+            cell?.msgTime.text = String(Int(diffTime)) + " minutes ago";
+        } else {
+            let timeString = String(describing: msgs[indexPath.row].timestamp);
+            cell?.msgTime.text = timeString.substring(to: timeString.index(timeString.startIndex, offsetBy: 19))
+        }
+        
+        cell?.msgText.text = msgs[indexPath.row].name! + " says: " + msgs[indexPath.row].message!
         
         return cell!
     }
