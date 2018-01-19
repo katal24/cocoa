4ae82e72c2557223c0be8dcb67ada7b8cfcea7dc
Student <student@imac-07.local>
2018-01-19 16:16:01 +0100

    my first commit

---
+++ cocoa/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  cocoa
+//
+//  Created by Student on 19/01/18.
+//  Copyright © 2018 KIS AGH. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
