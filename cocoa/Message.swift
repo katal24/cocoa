e2fb84892e75806650485c32c4d6405e12e519e8
Student <student@imac-07.local>
2018-01-19 16:26:25 +0100

    2

---
+++ cocoa/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  cocoa
+//
+//  Created by Student on 19/01/18.
+//  Copyright © 2018 KIS AGH. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+    var author: String?
+    var text: String?
+    
+    func setText(mess : String){
+        text = mess
+    }
+}
--- cocoa/MessageTableViewController.swift
+++ cocoa/MessageTableViewController.swift
@@ -36,6 +36,8 @@ class MessageTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
+    
+    
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
